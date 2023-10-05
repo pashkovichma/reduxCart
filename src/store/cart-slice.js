130cab41aeeb2969a4d50de1c1212817aa34bebb
Maria <pashkovichma@gmail.com>
2023-10-05 12:14:08 +0200

    useeffect with redux

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -7,9 +7,13 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
@@ -17,7 +21,7 @@ const cartSlice = createSlice({
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
       } else {
         existingItem.quantity++;
